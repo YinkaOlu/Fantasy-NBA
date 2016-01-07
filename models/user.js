eb21394c844932cc5a5be105f9fe4490e1a73edf
YinkaOlu <yinka.olu1@gmail.com>
2016-01-06 20:16:20 -0500

    Add roles attribute to User model

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema ({
     username: String,
     password: String,
-    email: String
+    email: String,
+    role: {type: String, default: 'basic'}
 });
 
 module.exports = mongoose.model("User", userSchema);
