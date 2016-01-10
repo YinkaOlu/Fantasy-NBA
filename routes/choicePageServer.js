465c25d545e240e1beb764aaee11177ff13bf307
YinkaOlu <yinka.olu1@gmail.com>
2016-01-10 03:05:09 -0500

    Small Path Change

--- routes/choicePageServer.js
+++ routes/choicePageServer.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('choicePage/choicePage', { title: 'Choice Page' });
+    res.render('ChoicePage/choicePage', { title: 'Choice Page' });
 });
 
 
