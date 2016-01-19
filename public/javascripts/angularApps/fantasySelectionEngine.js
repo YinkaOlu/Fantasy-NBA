7ff966e070f9abfa2ef2543740c130707f7c3c74
Yinka Olu <yinka.olu1@gmail.com>
2016-01-19 11:15:19 -0500

    Add Loading Bar + Fantasy Logic

--- public/javascripts/angularApps/fantasySelectionEngine.js
+++ public/javascripts/angularApps/fantasySelectionEngine.js
@@ -3,8 +3,49 @@
  */
 var app = angular.module('fantasySelectionEngine',['ngMaterial']);
 
-app.controller('fantasySelectionEngine', ['$scope', '$http',
-    function($scope, $http) {
+app.controller('fantasySelectionEngine', ['$scope', '$http', '$interval',
+    function($scope, $http, $interval) {
+
+        $scope.hideResultsLoading = true;
+        $scope.hideSelectionLoading = true;
+        //----------------------------------------------
+        //---------------------------------------------
+        var self = this, j= 0, counter = 0;
+        self.mode = 'query';
+        self.activated = true;
+        self.determinateValue = 30;
+        self.determinateValue2 = 30;
+        self.modes = [ ];
+        /**
+         * Turn off or on the 5 themed loaders
+         */
+        self.toggleActivation = function() {
+            if ( !self.activated ) self.modes = [ ];
+            if (  self.activated ) {
+                j = counter = 0;
+                self.determinateValue = 30;
+                self.determinateValue2 = 30;
+            }
+        };
+        $interval(function() {
+            self.determinateValue += 1;
+            self.determinateValue2 += 1.5;
+            if (self.determinateValue > 100) self.determinateValue = 30;
+            if (self.determinateValue2 > 100) self.determinateValue2 = 30;
+            // Incrementally start animation the five (5) Indeterminate,
+            // themed progress circular bars
+            if ( (j < 2) && !self.modes[j] && self.activated ) {
+                self.modes[j] = (j==0) ? 'buffer' : 'query';
+            }
+            if ( counter++ % 4 == 0 ) j++;
+            // Show the indicator in the "Used within Containers" after 200ms delay
+            if ( j == 2 ) self.contained = "indeterminate";
+        }, 100, 0, true);
+        $interval(function() {
+            self.mode = (self.mode == 'query' ? 'determinate' : 'query');
+        }, 7200, 0, true);
+        //----------------------------------------------
+        //----------------------------------------------
 
         //HTTP Call to nba database, Retrieve team list
         $http.get('/api/team').success(function(response){
@@ -299,6 +340,7 @@ app.controller('fantasySelectionEngine', ['$scope', '$http',
                         }
 
                     });
+
 $scope.hidePlayerSelection = false;
             $scope.assignedPlayer = {};
         };
@@ -315,6 +357,7 @@ $scope.hidePlayerSelection = false;
 
         //----------- Add Player to Fantasy Roster ---------------
         $scope.addPlayer = function () {
+            $scope.hideSelectionLoading = false;
             //Store input Player
             var playerToAdd = $scope.assignedPlayer;
 
@@ -389,6 +432,7 @@ $scope.hidePlayerSelection = false;
             $scope.hideTeam = false;
             $scope.disableBuildBtn = false;
             $scope.hidePlayerSelection = true;
+            $scope.hideSelectionLoading = true;
         };
         $scope.saveTeam = function(){
             //HTTP Call to server, Retrieve User Info
@@ -469,9 +513,16 @@ $scope.hidePlayerSelection = false;
         };
 
         $scope.dblStats = function(){
+            $scope.hideRoster = true;
             $scope.getStats();
             $scope.getStats();
 
+        };
+        $scope.backToRoster = function(){
+            $scope.hideRoster = false;
+
+            $scope.hideResults = true;
+            $scope.hideStatsBtn = false;
         };
 
         $scope.getStats = function() {
@@ -943,6 +994,7 @@ $scope.hidePlayerSelection = false;
         // Instantiate and draw Points chart
         var chartFoul = new google.visualization.ColumnChart(document.getElementById('FoulChart'));
         chartFoul.draw(FoulData, FoulOptions);
+            $scope.hideResultsLoading = true;
     };
 
     }]);
--- public/javascripts/angularApps/playerStatApp.js
+++ public/javascripts/angularApps/playerStatApp.js
@@ -3,8 +3,48 @@
  */
 var app = angular.module('playerStatApp',['ngMaterial']);
 
-app.controller('playerStatCtrl', ['$scope', '$http',
-    function($scope, $http){
+app.controller('playerStatCtrl', ['$scope', '$http','$interval',
+    function($scope, $http, $interval){
+
+        $scope.hideResultsLoading = true;
+        //----------------------------------------------
+        //---------------------------------------------
+        var self = this, j= 0, counter = 0;
+        self.mode = 'query';
+        self.activated = true;
+        self.determinateValue = 30;
+        self.determinateValue2 = 30;
+        self.modes = [ ];
+        /**
+         * Turn off or on the 5 themed loaders
+         */
+        self.toggleActivation = function() {
+            if ( !self.activated ) self.modes = [ ];
+            if (  self.activated ) {
+                j = counter = 0;
+                self.determinateValue = 30;
+                self.determinateValue2 = 30;
+            }
+        };
+        $interval(function() {
+            self.determinateValue += 1;
+            self.determinateValue2 += 1.5;
+            if (self.determinateValue > 100) self.determinateValue = 30;
+            if (self.determinateValue2 > 100) self.determinateValue2 = 30;
+            // Incrementally start animation the five (5) Indeterminate,
+            // themed progress circular bars
+            if ( (j < 2) && !self.modes[j] && self.activated ) {
+                self.modes[j] = (j==0) ? 'buffer' : 'query';
+            }
+            if ( counter++ % 4 == 0 ) j++;
+            // Show the indicator in the "Used within Containers" after 200ms delay
+            if ( j == 2 ) self.contained = "indeterminate";
+        }, 100, 0, true);
+        $interval(function() {
+            self.mode = (self.mode == 'query' ? 'determinate' : 'query');
+        }, 7200, 0, true);
+        //----------------------------------------------
+        //----------------------------------------------
 
         $scope.hideTeams = false;
         $scope.hideRoster = true;
@@ -74,11 +114,13 @@ app.controller('playerStatCtrl', ['$scope', '$http',
         };
 
         $scope.backToRoster = function(){
+            $scope.hideResultsLoading = true;
             $scope.hideResults = true;
             $scope.hideRoster = false;
         };
 
         $scope.directResults = function(playerPos){
+            $scope.hideResultsLoading = false;
             $scope.player = $scope.roster[playerPos];
             $scope.playerID = $scope.player._id;
             $scope.displayResults();
@@ -483,7 +525,10 @@ app.controller('playerStatCtrl', ['$scope', '$http',
             // Instantiate and draw Points chart
             var chart = new google.visualization.AreaChart(document.getElementById('FoulChart'));
             chart.draw(data, options);
+            $scope.hideResultsLoading = true;
         };
+
+
 
 
 
