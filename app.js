7c5ce2d5e012fcaca4e362825eeee13a847d5a03
yinka <yinka.olu1@gmail.com>
2016-09-15 02:12:31 -0400

    dev settings

--- app.js
+++ app.js
@@ -39,5 +39,47 @@ app.use('/choicePage', choicePageRoute);
 app.use('/api', api);
 app.use('/calculate', calculateRoute);
 
+//Set port to listen to
+var port = process.env.PORT || 8080;
+app.listen(port);
+
+
+
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 
 module.exports = app;
